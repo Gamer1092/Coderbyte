13ae47f9f0d6c662bd691084fde7340e6690618d
Gamer1092 <lynjd@yahoo.ca>
2015-12-01 21:46:35 -0500

    Create First Reverse.cpp
    Using the function FirstReverse (str), take the str parameter being passed and return the string in reverse order

---
+++ First Reverse.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+using namespace std;
+
+string FirstReverse(string str) { 
+
+  // code goes here   
+  // Using a for loop, append the original string to the new reversed string
+  // starting from the back
+  string reversedString = "";
+  for(int i = str.length() - 1; i >= 0; i--)
+    reversedString = reversedString + str[i];
+  return reversedString; 
+            
+}
+
+int main() { 
+  
+  // keep this function call here
+  cout << FirstReverse(gets(stdin));
+  return 0;
+    
+}           
