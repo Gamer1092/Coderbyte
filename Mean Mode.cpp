6fadfa7216f6af388c80dd1f0f959800ba8e6961
Gamer1092 <lynjd@yahoo.ca>
2015-12-02 15:15:11 -0500

    Create Mean Mode.cpp
    Have the function MeanMode (arr) and take the numbers stored in arr and return 1 if the mode equal the mean and 0 if otherwise.

---
+++ Mean Mode.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int MeanMode(int arr[]) { 
+
+  // code goes here 
+  int numElements = sizeof(arr)/sizeof(arr[0]);
+  int sum = 0;
+  int max = 1;
+  int count = 1;
+  int mean;
+  int mode;
+  
+  sort(arr, arr + numElements);
+  int first = arr[0];
+  sum += first;
+  
+  for(int i = 1; i < numElements; i++){
+    sum += arr[i];
+    if(arr[i] == first){
+      count += 1;
+    }
+    else{
+      first = arr[i];
+    }
+    
+    if(count > max){
+     mode = arr[i]; 
+     max = count;
+     count = 1;
+    } 
+  }
+  
+  mean = sum/numElements;
+  if(mean == mode){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+  return 0; 
+            
+}
+
+int main() { 
+   
+  // keep this function call here
+  /* Note: In C++ you first have to initialize an array and set 
+     it equal to the stdin to test your code with arrays. 
+     To see how to enter arrays as arguments in C++ scroll down */
+     
+  int A[] = gets(stdin);
+  cout << MeanMode(A);
+  return 0;
+    
+}           
